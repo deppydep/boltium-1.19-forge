6f28580260afdb4d39b9989e251c10326aaf9747
depi_depi <akendep@gmail.com>
2022-08-06 18:02:26 +0530

    update

--- src/main/java/me/depi/boltium/boltium.java
+++ src/main/java/me/depi/boltium/boltium.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+
 @Mod(boltium.MODID)
 public class boltium
 {
